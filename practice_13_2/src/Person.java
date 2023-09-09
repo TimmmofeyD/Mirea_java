b0a0e1ef58f9d9c6ed8dbe62e295fdd867863604
TimmmofeyD <timmmofey@itachi-uchiha.ru>
2023-09-09 09:50:35 +0300

    Add practice 13_2

---
+++ practice_13_2/src/Person.java
@@ -0,0 +1,39 @@
+import java.util.Objects;
+
+public class Person {
+    private String firstName = "";
+    private String lastName = "";
+    private String maleName = "";
+
+    public void setFirstName(String first_name) {
+        if (!Objects.equals(first_name, "")) {
+            this.firstName = first_name;
+        } else {
+            this.firstName = " ";
+        }
+    }
+
+    public void setLastName(String last_name) {
+        this.lastName = last_name;
+    }
+
+    public void setMaleName(String male_name) {
+        if (!Objects.equals(male_name, "")) {
+            this.maleName = male_name;
+        } else {
+            this.maleName = " ";
+        }
+    }
+
+    public String getInfo() {
+        return lastName + " " + firstName.charAt(0) + " " + maleName.charAt(0);
+    }
+
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setLastName("Jumba");
+        person.setFirstName("");
+        person.setMaleName("");
+        System.out.println(person.getInfo());
+    }
+}
