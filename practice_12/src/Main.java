2164b0108b5a32bfebb52c8d436362adb3694cba
TimmmofeyD <timmmofey@itachi-uchiha.ru>
2023-09-09 09:12:01 +0300

    Add practice 11_5, 12

---
+++ practice_12/src/Main.java
@@ -0,0 +1,169 @@
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.util.Objects;
+
+// App class
+class MyApp extends JFrame {
+    int WINDOW_WIDTH = 600;
+    int WINDOW_HEIGHT = 600;
+    String background_image_path;
+    String animation_images_path = "../frames/";
+    int method;
+
+    // Init
+    MyApp() {
+        super("Some shapes");
+        setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        //setBackground(Color.BLACK);
+        setLocation(300, 300);
+        setLayout(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        this.method = 0;
+
+        // Btn "Start animation"
+        Button btn = new Button("Start animation");
+        btn.setSize(200, 100);
+        btn.setLocation(0, 0);
+        btn.addActionListener(
+                event -> {
+                    method = 1;
+                    setTitle("Animation");
+                    setSize(WINDOW_WIDTH + 1, WINDOW_HEIGHT);
+                    setSize(WINDOW_WIDTH - 1, WINDOW_HEIGHT);
+                }
+        );
+        add(btn);
+    }
+
+    void set_background_image_path(String path) {
+        this.background_image_path = path;
+    }
+
+    void set_animation_images_path(String path) {
+        this.animation_images_path = path;
+    }
+
+    // Paint method
+    @Override
+    public void paint(Graphics g2) {
+        Graphics2D g = (Graphics2D) g2;
+        Image img = Toolkit.getDefaultToolkit().getImage(this.background_image_path);
+        g.drawImage(img, 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, this);
+
+        switch (this.method) {
+            case 0 -> random_shapes_paint(g);
+            case 1 -> animate(this.animation_images_path, g);
+            default -> {
+            }
+        }
+    }
+
+    // Random shapes paint method
+    void random_shapes_paint(Graphics2D g) {
+        for (int i = 0; i < 50; i++) {
+            int choice = (int) (Math.random() * 7);
+
+            switch (choice) {
+
+                // Oval
+                case 1 -> {
+                    g.setColor(new Color((int) (Math.random() * 0x1000000)));
+                    g.fillOval(
+                            (int) (Math.random() * WINDOW_WIDTH),
+                            (int) (Math.random() * WINDOW_HEIGHT),
+                            (int) (Math.random() * 200),
+                            (int) (Math.random() * 200)
+                    );
+                }
+
+                // Rect
+                case 2 -> {
+                    g.setColor(new Color((int) (Math.random() * 0x1000000)));
+                    g.fillRect(
+                            (int) (Math.random() * WINDOW_WIDTH),
+                            (int) (Math.random() * WINDOW_HEIGHT),
+                            (int) (Math.random() * 200),
+                            (int) (Math.random() * 200)
+                    );
+                }
+
+                // Line
+                case 3 -> {
+                    g.setColor(new Color((int) (Math.random() * 0x1000000)));
+                    g.drawLine(
+                            (int) (Math.random() * WINDOW_WIDTH),
+                            (int) (Math.random() * WINDOW_WIDTH),
+                            (int) (Math.random() * WINDOW_HEIGHT),
+                            (int) (Math.random() * WINDOW_HEIGHT)
+                    );
+                }
+
+                // Arc
+                case 4 -> {
+                    g.setColor(new Color((int) (Math.random() * 0x1000000)));
+                    g.drawArc(
+                            (int) (Math.random() * WINDOW_WIDTH),
+                            (int) (Math.random() * WINDOW_HEIGHT),
+                            (int) (Math.random() * WINDOW_WIDTH),
+                            (int) (Math.random() * WINDOW_HEIGHT),
+                            (int) (Math.random() * 360),
+                            (int) (Math.random() * 360)
+                    );
+                }
+                // Rounded rect
+                case 5 -> {
+                    g.setColor(new Color((int) (Math.random() * 0x1000000)));
+                    g.fillRoundRect(
+                            (int) (Math.random() * WINDOW_WIDTH),
+                            (int) (Math.random() * WINDOW_HEIGHT),
+                            (int) (Math.random() * 200),
+                            (int) (Math.random() * 200),
+                            (int) (Math.random() * 200),
+                            (int) (Math.random() * 200)
+                    );
+                }
+
+                // Circle
+                case 6 -> {
+                    g.setColor(new Color((int) (Math.random() * 0x1000000)));
+                    g.fillOval(
+                            (int) (Math.random() * WINDOW_WIDTH),
+                            (int) (Math.random() * WINDOW_HEIGHT),
+                            100,
+                            100
+                    );
+                }
+                default -> {
+                }
+            }
+        }
+    }
+
+    void animate(String frames_path, Graphics2D g) {
+        File dir = new File(frames_path);
+        for (File file : Objects.requireNonNull(dir.listFiles())) {
+            Image frame = Toolkit.getDefaultToolkit().getImage(file.getPath());
+            g.drawImage(frame, 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, this);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException ignored) {
+            }
+        }
+    }
+
+    // Start app
+    public static void main(String[] args) {
+        MyApp app = new MyApp();
+        if (args.length == 1) {
+            app.set_background_image_path(args[0]);
+        } else if (args.length == 2) {
+            app.set_background_image_path(args[0]);
+            app.set_animation_images_path(args[1]);
+        } else {
+            System.out.println("Background image path is empty");
+        }
+
+    }
+}
