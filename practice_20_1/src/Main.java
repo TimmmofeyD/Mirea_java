ae41c3791af403fb97e5216c16db229b5bde4b3f
TimmmofeyD <timmmofey@itachi-uchiha.ru>
2023-09-27 13:27:15 +0300

    add 20_1, mod 11_1, update doc

---
+++ practice_20_1/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    @Override
+    public String toString() {
+        return "Main{}";
+    }
+
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+
+    }
+}
